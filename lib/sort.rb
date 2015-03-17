2220c3b75e4610abc9436327dbf108375b840703
Michael Pospisil <michaelcpospisil@gmail.com>
2015-03-16 18:05:23 -0700

    adding solution for ruby bubble sort

--- lib/sort.rb
+++ lib/sort.rb
@@ -1,6 +1,18 @@
 class Array
-
-  def bubble_sort
+  # array = [5,3,2,1,4]
+  def bubble_sort()
+    loop do
+      swapped = false
+   
+      ((self.length)-1).times do |i|
+        if self[i] > self[i+1]
+          self[i], self[i+1] = self[i+1], self[i]
+          swapped = true
+        end
+      end
+   
+      break if not swapped
+    end
+    self
   end
-
 end
